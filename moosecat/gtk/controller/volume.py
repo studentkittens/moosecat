7f3b23e321f7f890e8406ac0888c8a6362203650
Chris P <sahib@online.de>
2013-08-25 19:19:36 +0200

    Implemented life behind VolumeSlider (missing TODO points though there)

--- moosecat/gtk/controller/volume.py
+++ moosecat/gtk/controller/volume.py
@@ -1,6 +1,7 @@
 from gi.repository import Gtk, GLib
 from moosecat.gtk.interfaces import Hideable
 from moosecat.gtk.widgets import BarSlider
+from moosecat.core import Idle
 from moosecat.boot import g
 
 
@@ -9,3 +10,18 @@ class Volume(Hideable):
         self._vol_align = builder.get_object('volumeslider_align')
         self._volume_bar = BarSlider()
         self._vol_align.add(self._volume_bar)
+
+        g.client.signal_add('client-event', self._on_client_event)
+
+        # TODO: Use gsignals like everyone does.
+        self._volume_bar.on_percent_change_func = self._on_click_event
+
+    def _on_client_event(self, client, event):
+        if event & Idle.MIXER:
+            with client.lock_status() as status:
+                self._volume_bar.percent = status.volume / 100
+
+    def _on_click_event(self, bar):
+        # TODO: This is stupid.
+        with g.client.lock_status() as status:
+            status.volume = bar.percent * 100
