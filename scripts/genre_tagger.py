33397857dd12d3800548758188431619d8a89031
Chris P <sahib@online.de>
2013-12-29 14:11:41 +0100

    Document genre tagger a lil bit

--- scripts/genre_tagger.py
+++ scripts/genre_tagger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-API_SEARCH = "http://api.discogs.com/database/search?type=release&q={artist}"
+API_SEARCH_URL = "http://api.discogs.com/database/search?type=release&q={artist}"
 
 
 import json
@@ -12,64 +12,89 @@ from urllib.request import urlopen
 from urllib.parse import quote
 
 
-from munin.provider.normalize import ArtistNormalizeProvider, AlbumNormalizeProvider
+from munin.provider.normalize import \
+    ArtistNormalizeProvider, \
+    AlbumNormalizeProvider
 
 
 # External:
-from pyxdameraulevenshtein import normalized_damerau_levenshtein_distance as levenshtein
+from pyxdameraulevenshtein import normalized_damerau_levenshtein_distance as \
+    levenshtein
 import taglib
-
-
-def write_genre_tag(music_path, genre_value):
-    handle = taglib.File(music_path)
-    handle.tags['GENRE'] = genre_value or ''
-    failures = handle.save()
-    if failures:
-        print('Warning: could not write tags ({}) on: {}'.format(
-            failures, music_path
-        ))
 
 
 def find_genre_via_discogs(artist, album):
     genre_set, style_set = set(), set()
 
-    api_root = API_SEARCH.format(artist=quote(artist))
+    # Get the data from discogs
+    api_root = API_SEARCH_URL.format(artist=quote(artist))
     html_doc = urlopen(api_root).read().decode('utf-8')
     json_doc = json.loads(html_doc)
 
+    # Normalize the input artist/album
     artist_normalizer = ArtistNormalizeProvider()
     album_normalizer = AlbumNormalizeProvider()
-
     artist, *_ = artist_normalizer.do_process(artist)
     album, *_ = album_normalizer.do_process(album)
 
     def find_right_genre(persist_on_album):
         for item in json_doc['results']:
+            # Some artist items have not a style in them.
+            # Skip these items.
             if 'style' not in item:
                 continue
 
             # print('   ', item['title'], item['genre'], item['style'])
+            # Get the remote artist/album from the title, also normalise them.
             remote_artist, remote_album = item['title'].split(' - ', maxsplit=1)
             remote_artist, *_ = artist_normalizer.do_process(remote_artist)
             remote_album, *_ = album_normalizer.do_process(remote_album)
 
+            # Try to outweight spelling errors, or small
+            # pre/suffixes to the artist. (i.e. 'the beatles' <-> beatles')
             if levenshtein(artist, remote_artist) > 0.5:
                 continue
 
+            # Same for the album:
             if persist_on_album and levenshtein(album, remote_album) > 0.5:
                 continue
 
+            # Remember the set of all genres and styles.
             genre_set.update(item['genre'])
             style_set.update(item['style'])
 
     find_right_genre(True)
     if not (genre_set or style_set):
+        # Lower the expectations, just take the genre of
+        # all known albums of this artist, if any:
         find_right_genre(False)
 
+    # Still not? Welp.
     if not (genre_set or style_set):
         return None
 
-    return (' / '.join(('; '.join(genre_set), ', '.join(style_set))))
+    # Bulid a genre string that is formatted this way:
+    #  genre1; genre2 [;...] / style1, style2, style3 [,...]
+    #  blues; rock / blues rock, country rock, christian blues
+    return ' / '.join(('; '.join(genre_set), ', '.join(style_set)))
+
+
+def write_genre_tag(music_path, genre_value):
+    # Open the via taglib:
+    handle = taglib.File(music_path)
+
+    # genre_value might be None. In this case just clear this tag.
+    if genre_value is not None:
+        # Existing genre tag gets overwritten.
+        handle.tags['GENRE'] = genre_value
+    elif 'GENRE' in handle.tags:
+        del handle.tags['GENRE']
+
+    failures = handle.save()
+    if failures:
+        print('Warning: could not write tags ({}) on: {}'.format(
+            failures, music_path
+        ))
 
 
 def tag_music(data_mapping):
@@ -100,15 +125,19 @@ def tag_music(data_mapping):
 
 
 if __name__ == '__main__':
+    # Stdlib:
     import os
     from collections import defaultdict
 
-    import moosecat.core as m
+    # External:
+    import moosecat.core
 
-    client = m.Client()
+    client = moosecat.core.Client()
     client.connect()
     client.store_initialize('/tmp')
 
+    # Collect a whole-lot-of data from the database in this format:
+    # {('artist', 'album'): set(['/home/a/foo.mp3', # '/home/piotr/blub.mp3'])}
     data_mapping = defaultdict(set)
     with client.store.query('*') as full_playlist:
         for song in full_playlist:
@@ -117,3 +146,4 @@ if __name__ == '__main__':
             data_mapping[(artist, song.album)].add(full_path)
 
     tag_music(data_mapping)
+    client.disconnect()
