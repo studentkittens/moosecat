d31588d900dfbaa01dcce52d383504d4ca0dae72
Chris P <sahib@online.de>
2014-03-02 17:11:14 +0100

    Playing around with the sidebar

--- moosecat/plugins/gtk/browser_queue.py
+++ moosecat/plugins/gtk/browser_queue.py
@@ -1,7 +1,7 @@
 from moosecat.gtk.widgets import PlaylistTreeModel, PlaylistWidget, SimplePopupMenu
 from moosecat.plugins import IGtkBrowser
 from moosecat.boot import g
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 
 
 class QueuePlaylistWidget(PlaylistWidget):
@@ -49,6 +49,43 @@ class QueuePlaylistWidget(PlaylistWidget):
         print(menu_item)
 
 
+from moosecat.gtk.utils import plyr_cache_to_pixbuf
+from moosecat.metadata import configure_query_by_current_song, update_needed
+from moosecat.core import Idle, Status
+from moosecat.gtk.utils import add_keybindings
+
+
+class SidebarCover(Gtk.Image):
+    def __init__(self):
+        Gtk.Image.__init__(self)
+
+        self.set_size_request(250, 250)
+        g.client.signal_add('client-event', self._on_client_event)
+        self._last_query = None
+
+    def _on_client_event(self, client, event):
+        if not event & Idle.PLAYER:
+            return
+
+        qry = configure_query_by_current_song('cover')
+        if not update_needed(self._last_query, qry):
+            # LOGGER.debug('sidebar cover needs no update')
+            return
+
+        self._submit_tmstmp = g.meta_retriever.push(
+            qry,
+            self._on_item_retrieved
+        )
+
+    def _on_item_retrieved(self, order):
+        if order.results and self._submit_tmstmp <= order.timestamp:
+            cache, *_ = order.results
+            pixbuf = plyr_cache_to_pixbuf(cache, width=150, height=150)
+            if pixbuf is not None:
+                self.set_from_pixbuf(pixbuf)
+                self._last_query = order.query
+
+
 class QueueBrowser(IGtkBrowser):
     def do_build(self):
         self._scw = Gtk.ScrolledWindow()
@@ -58,7 +95,35 @@ class QueueBrowser(IGtkBrowser):
         )
 
         self._scw.add(QueuePlaylistWidget())
-        self._scw.show_all()
+        self._overlay = Gtk.Overlay()
+        self._overlay.add(self._scw)
+
+        self._box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self._label = Gtk.Label('<span bgcolor="#AAAAAA"><b>   Metadata:   </b></span>\n(Use Ctrl-T to toggle)')
+        self._label.set_use_markup(True)
+        self._label.set_size_request(250, -1)
+        self._label.override_background_color(0, Gdk.RGBA(0.5, 0.5, 1.0, 0.2))
+        self._label.set_halign(Gtk.Align.END)
+        self._label.set_alignment(0.5, 0.4)
+        self._box.set_halign(Gtk.Align.END)
+
+        self._box.pack_start(SidebarCover(), True, True, 10)
+
+        self._overlay.add_overlay(self._label)
+        self._overlay.add_overlay(self._box)
+        self._overlay.show_all()
+
+        add_keybindings(self._scw.get_child(), {
+            '<Ctrl>t':  self._on_toggle_sidebar
+        })
+
+    def _on_toggle_sidebar(self, playlist, key):
+        if self._label.is_visible():
+            self._label.hide()
+            self._box.hide()
+        else:
+            self._label.show()
+            self._box.show_all()
 
     #######################
     #  IGtkBrowser Stuff  #
@@ -66,7 +131,7 @@ class QueueBrowser(IGtkBrowser):
 
     def get_root_widget(self):
         'Return the root widget of your browser window.'
-        return self._scw
+        return self._overlay
 
     def get_browser_name(self):
         'Get the name of the browser (displayed in the sidebar)'
