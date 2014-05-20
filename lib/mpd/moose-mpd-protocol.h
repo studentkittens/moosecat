d00721df4deafbf7a7e6a9989fc5f37a78618971
Chris P <sahib@online.de>
2014-05-20 18:56:15 +0200

    Removed dead prototypes.

--- lib/mpd/moose-mpd-protocol.h
+++ lib/mpd/moose-mpd-protocol.h
@@ -370,46 +370,6 @@ void moose_status_timer_unregister(
  */
 bool moose_status_timer_is_active(MooseClient * self);
 
-
-/**
- * @brief Check if an output is enabled.
- *
- * @param self the client to operate on
- * @param output_name a user defined string of the output (like in mpd.conf)
- *
- * @return -1 on "no such name", 0 if disabled, 1 if enabled
- */
-bool moose_outputs_get_state(MooseClient * self, const char * output_name);
-
-/**
- * @brief Get a list of known output names
- *
- * @param data the client to operate on
- *
- * @return a NULL terminated list of names,
- *         free the list (not the contents) with free() when done
- */
-const char * * moose_outputs_get_names(MooseClient * self);
-
-/**
- * @brief Set the state of a Output (enabled or disabled)
- *
- * This can also be accomplished with:
- *
- *    moose_client_send("output_switch horst 1");
- *
- *  to enable the output horst. This is just a convinience command.
- *
- * @param data the client to operate on
- * @param output_name The name of the output to alter
- * @param state the state, True means enabled.
- *              If nothing would change no command is send.
- *
- * @return True if an output with this name could be found.
- */
-bool moose_outputs_set_state(MooseClient * self, const char * output_name, bool state);
-
-
 MooseStatus * moose_ref_status(MooseClient * self);
 
 
